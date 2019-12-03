1b868887c93b1ced2162cb3d3003a23f6fcc6696
chazdada <sdlwscz@hotmail.com>
2019-12-03 21:25:34 +0800

    spu属性查询完成

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/impl/SpuServiceImpl.java
@@ -0,0 +1,26 @@
+package com.atguigu.gmall.manage.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.manage.mapper.SpuInforMapper;
+import com.atguigu.gmall.service.SpuService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class SpuServiceImpl implements SpuService {
+
+    @Autowired
+    SpuInforMapper spuInforMapper;
+
+    @Override
+    public List<PmsProductInfo> spuList(String catalog3Id) {
+        PmsProductInfo pmsProductInfo = new PmsProductInfo();
+        pmsProductInfo.setCatalog3Id(catalog3Id);
+
+        List<PmsProductInfo> pmsProductInfos = spuInforMapper.select(pmsProductInfo);
+
+        return pmsProductInfos;
+    }
+}
